20d290785e5f7775b7097160d33f37bf43b3b7a1
Pierre <pierre@antilop.fr>
2020-11-03 13:01:00 +0100

    Bitbag Api Tester command

--- src/Command/ApiTesterCommand.php
+++ src/Command/ApiTesterCommand.php
@@ -6,12 +6,12 @@ use Doctrine\ORM\EntityManager;
 use OAuth2\OAuth2;
 use Sylius\Bundle\AdminApiBundle\Model\Client;
 use Sylius\Component\Core\Model\AdminUser;
-use Sylius\Component\User\Security\UserPasswordEncoderInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ApiTesterCommand extends Command
 {
@@ -47,79 +47,39 @@ class ApiTesterCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->em = $this->container->get('doctrine')->getManager();
-        $client_repository = $this->em->getRepository(Client::class);
-        $admin_repository = $this->em->getRepository(AdminUser::class);
+        $this->getAccessToken();
+        if($this->access_token_data) {
+            $products = $this->getProducts();
 
-        $client = $client_repository->find(1);
-        $api_user = $admin_repository->findOneBy(['username' => 'api']);
-        $this->getAccessToken($client, $api_user);
-        $products = $this->getProducts();
+            if (!is_null($products)) {
+                $output->writeln('products count: ' . $products->hits->total);
+                $output->writeln('first product data: ' . json_encode($products->hits->hits[0]));
 
-        if (!is_null($products)) {
-            $output->writeln('current page number: ' . $products->page);
-            $output->writeln('search limit: ' . $products->limit);
-            $output->writeln('number of pages: ' . $products->pages);
-            $output->writeln('products count: ' . $products->total);
-            $output->writeln('first product data: ' . json_encode($products->_embedded->items[0]));
-
-            $output->writeln('<info>Api is available</info>');
-            return 0;
+                $output->writeln('<info>Api is available</info>');
+                return 0;
+            }
         }
 
         // something wrong during requests
+        $output->writeln('<error>Api Oauth failed</error>');
         return 1;
     }
 
-    protected function getAccessToken($client, $api_user)
+    protected function getAccessToken()
     {
-        if (is_null($client)) {
-            $client = new Client();
-            $client->setAllowedGrantTypes([
-                OAuth2::GRANT_TYPE_IMPLICIT,
-                OAuth2::GRANT_TYPE_USER_CREDENTIALS,
-                OAuth2::GRANT_TYPE_REFRESH_TOKEN
-            ]);
-
-            $this->em->persist($client);
-            $this->em->flush();
-        }
-
-        if (is_null($api_user)) {
-            // Création du user API si les fixtures n'ont pas été chargées.
-            $api_user = new AdminUser();
-            $api_user->setEmail('api@example.com');
-            $api_user->setUsername('api');
-            $api_user->setPlainPassword('sylius-api');
-            $api_user->setPassword($this->userPasswordEncoder->encode($api_user));
-            $api_user->eraseCredentials();
-            $api_user->setEnabled(true);
-            $api_user->setLocaleCode('%locale%');
-            $api_user->setFirstName('Luke');
-            $api_user->setLastName('Brushwood');
-            $api_user->addRole('ROLE_ADMINISTRATION_ACCESS');
-            $api_user->addRole('ROLE_API_ACCESS');
-
-            $this->em->persist($api_user);
-            $this->em->flush();
-        }
-
-        $public_id = $client->getPublicId();
-        $secret = $client->getSecret();
-
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => $this->router->generate('fos_oauth_server_token', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            CURLOPT_URL => $this->router->generate('bitbag_vue_storefront_plugin_user_login', [], UrlGeneratorInterface::ABSOLUTE_URL),
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => [
-                'client_id' => $public_id,
-                'client_secret' => $secret,
-                'grant_type' => 'password',
-                'username' => $api_user->getUsername(),
-                'password' => $api_user->getPassword()
+            CURLOPT_POSTFIELDS => json_encode([
+                "username" => "shop@example.com",
+                "password" => "sylius"
+            ]),
+            CURLOPT_HTTPHEADER => [
+                "Content-Type: application/json",
             ],
         ]);
 
@@ -127,7 +87,11 @@ class ApiTesterCommand extends Command
 
         curl_close($curl);
 
-        $this->access_token_data = json_decode($response);
+        $data = json_decode($response);
+
+        if (!is_null($data) && $data->code === 200) {
+            $this->access_token_data = $data->result;
+        }
     }
 
     protected function getProducts()
@@ -135,7 +99,7 @@ class ApiTesterCommand extends Command
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => $this->router->generate('sylius_admin_api_product_index', ['version' => 2], UrlGeneratorInterface::ABSOLUTE_URL),
+            CURLOPT_URL => $this->router->generate('bitbag_vue_storefront_plugin_catalog_get_type', ['index' => getenv('ELASTICSEARCH_INDEX'), 'type'=> 'product'], UrlGeneratorInterface::ABSOLUTE_URL),
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
@@ -144,7 +108,7 @@ class ApiTesterCommand extends Command
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_HTTPHEADER => [
-                "Authorization: " . ucfirst($this->access_token_data->token_type) . ' ' . $this->access_token_data->access_token
+                "Authorization: Bearer " . $this->access_token_data
             ],
         ]);
 
